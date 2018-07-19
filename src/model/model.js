f5209b4cacaeb3e073fde5a5d8a70e0bf9811f50
chaoyi1987 <chaoyi1987@foxmail.com>
2018-07-19 18:33:34 +0800

    新增文件

---
+++ src/model/model.js
@@ -0,0 +1,12 @@
+function model(){
+  $(".model1").scc=({ "margin-top":"0.1rem",
+  "border": "0.2rem solid #ffffff"})
+  var sex = 1;
+  console.log($(this.store.getCookie('code')));
+  console.log();
+
+  if($(this.store.getCookie('code'))==0){
+    if(sex == 1){
+    }
+  }
+}
